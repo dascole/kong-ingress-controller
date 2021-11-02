51ac79b37fd5807b68bd514b01080d8f2038f8c9
dascole <72866937+dascole@users.noreply.github.com>
2021-11-02 12:28:30 -0400

    Delete cleanup.sh

--- KongIngress/cleanup.sh
+++
@@ -1,8 +0,0 @@
-
-kubectl config set-context --current --namespace cre
-kubectl delete -f https://bit.ly/sample-echo-service
-kubectl delete -f ../Ingress/simple-ingress.yaml
-kubectl delete -f kong-ingress-route-redirect.yaml
-kubectl delete -f kong-ingress-service-retries.yaml
-kubectl delete -f kong-ingress-hash-on-ip.yaml
-
